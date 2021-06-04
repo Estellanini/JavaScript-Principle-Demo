85085789f11c3b1ede149b647c33e2325bdc96dd
Estellanini <1357436238@qq.com>
2021-06-04 10:34:52 +0800

    feat: 新增原型原理练习

--- JavaScript-Advance/ProtoType-Demo.js
+++ JavaScript-Advance/ProtoType-Demo.js
@@ -32,3 +32,78 @@ console.log(Fun)//输出函数体
 //给原型对象添加属性（一般是方法）===>实例对象可以访问
 var fun = new Fun();
 fun.test();
+
+
+/*//任何对象都有属性__proto__
+const obj1 = {
+    'name':'jenny',
+    'num':'12345'
+}
+console.log(obj1)*/
+
+
+/*
+//__proto__对象原型和构造函数的原型对象prototype是等价的
+function Student(name,age){
+    this.name=name;
+    this.age=age;
+}
+var stu=new Student("jenny",20)
+console.log(stu)
+console.log("test1",stu.__proto__)
+console.log(Student.prototype)
+console.log("test2",stu.__proto__===Student.prototype)//true
+*/
+/*
+
+//扩展内置对象——求和
+console.log(Array.prototype)
+Array.prototype.sum=function(){
+    var sum=0;
+    for(var i=0;i<this.length;i++){
+        sum+=this[i];
+    }
+    return sum;
+}
+var arr=[1,2,3];
+console.log(arr.sum());
+*/
+
+/*
+
+//测试题1
+
+function A(){}
+A.prototype.n=1;
+console.log(A.prototype)//{n: 1, constructor: ƒ}
+var b=new A();
+
+A.prototype={
+    //constructor:A,    //手动添加构造函数
+    n:2,
+    m:3
+}
+console.log(A.prototype)//{n: 2, m: 3},这种是覆盖，如果不手动添加构造函数的话，是没有构造函数的
+var c=new A()
+console.log(b.n,b.m,c.n,c.m)//1 undefined 2 3
+
+*/
+
+//测试题2
+function F(){}
+Object.prototype.a=function(){
+    console.log('a()');
+}
+Function.prototype.b=function(){
+    console.log('b()');
+}
+var f=new F();
+console.log(f)//F {}
+console.log(Object.prototype)//{a: ƒ, constructor: ƒ, __defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, …}
+console.log(F.prototype)
+console.log('tttt',F.__proto__.b)//说明F.__proto__中有b,也有a
+/*f.a();//a()
+f.b();//报错,由于F.prototype能找到对象原型__proto__中的方法a,但没有方法b
+F.a();//a()
+F.b();//b()*/
+
