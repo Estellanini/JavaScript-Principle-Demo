1401fc8953728ccb64d18d69c870b93965ff3d12
Estellanini <1357436238@qq.com>
2021-06-14 11:06:16 +0800

    feat: 新增for in与for of区分

---
+++ JavaScript-Advance/for_in_for_of.js
@@ -0,0 +1,20 @@
+Object.prototype.objCustom = function() {};
+Array.prototype.arrCustom = function() {};
+
+let iterable = [3, 5, 7];
+iterable.foo = 'hello';
+
+console.log(iterable);
+for (let i in iterable) {
+    console.log(i); //  0, 1, 2, "foo", "arrCustom", "objCustom"
+}
+
+for (let i in iterable) {
+    if (iterable.hasOwnProperty(i)) {
+        console.log(i); // logs 0, 1, 2, "foo"
+    }
+}
+
+for (let i of iterable) {
+    console.log(i); // logs 3, 5, 7
+}
