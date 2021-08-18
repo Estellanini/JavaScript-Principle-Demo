c2f7197514672ed5a140708a9418c8db3355705c
Estellanini <1357436238@qq.com>
2021-08-18 10:18:34 +0800

    fix: this指向

--- JavaScript-Advance/this.js
+++ JavaScript-Advance/this.js
@@ -1,12 +1,24 @@
 //this指向原理
+/*
 
 console.log("全局",this)//window
 
 function foo(a,b){
-   // 'use strict'
+    'use strict'
     console.log("函数执行上下文 ",this)//window,在严格模式下指向undefined
     return a+b;
 }
 foo(1,2);
+*/
 
+
+var a = 10
+const b = 20
+
+function foo () {
+    console.log(this.a)
+    console.log(this.b)
+}
+foo();
+console.log(window.a)
 
