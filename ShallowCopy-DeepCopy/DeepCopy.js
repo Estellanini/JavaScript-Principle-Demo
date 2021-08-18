2067f7d16b2fec02b16080d8b66dd45b89af4c21
Estellanini <1357436238@qq.com>
2021-08-18 14:35:34 +0800

    fix: 修改错误

--- ShallowCopy-DeepCopy/DeepCopy.js
+++ ShallowCopy-DeepCopy/DeepCopy.js
@@ -64,7 +64,7 @@ function createData(deep, breadth) {
     return data;
 }
 
-/*const test=createData(3, 0); // 1层深度，每层有3个数据 {data: {0: 0, 1: 1, 2: 2}}
+/*const test=createData(1, 3); // 1层深度，每层有3个数据 {data: {0: 0, 1: 1, 2: 2}}
 createData(3, 0); // 3层深度，每层有0个数据 {data: {data: {data: {}}}}
 console.log(test)*/
 
